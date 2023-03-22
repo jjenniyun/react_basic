9b6be17bc80aea3e5614bcafa94fe176ad7bd83d
jjenniyun <hjkjj1847@gmail.com>
2023-03-22 14:31:52 +0900

    react_shop commit(23.03.22)

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,14 +1,48 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import data from './data.js';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <Button variant="success">Success</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">FastAnd</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Clothes</Nav.Link>
+            <Nav.Link href="#pricing">Community</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map((a, i) => {
+              return (
+                <Card shoes={shoes[i]} i={i}></Card>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
+}
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
+      <h5>{props.shoes.title}</h5>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
