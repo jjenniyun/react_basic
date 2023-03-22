38a1d5ddb6de5373d12995ad7096c61e8079eaca
jjenniyun <hjkjj1847@gmail.com>
2023-03-22 13:38:34 +0900

    new_react_part2 commit(23.03.22)

---
+++ shop/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="success">Success</Button>
+    </div>
+  );
+}
+
+export default App;
