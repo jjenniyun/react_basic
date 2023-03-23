953da8de1cf665b91fb85d47805635324b7fc605
jjenniyun <hjkjj1847@gmail.com>
2023-03-23 23:59:17 +0900

    router_commit(23.03.23)

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,11 +2,14 @@ import { useState } from "react";
 import './App.css';
 import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import data from './data.js';
+import Detail from './routes/Detail.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   let [shoes] = useState(data);
+  let navigate = useNavigate(); // 페이지 이동
 
   return (
     <div className="App">
@@ -14,28 +17,33 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">FastAnd</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
             <Nav.Link href="#features">Clothes</Nav.Link>
-            <Nav.Link href="#pricing">Community</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map((a, i) => {
-              return (
-                <Card shoes={shoes[i]} i={i}></Card>
-              )
-            })
-          }
-        </div>
-      </div>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                {shoes.map((a, i) => {
+                  return <Card shoes={shoes[i]} i={i} ></Card>
+                })}
+              </div>
+            </div>
+          </>
+        } />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
-function Card(props) {
+// 축약하고 싶은 UI 컴포넌트화 하기
+function Card(props) { // props 파라미터
   return (
     <div className="col-md-4">
       <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" />
